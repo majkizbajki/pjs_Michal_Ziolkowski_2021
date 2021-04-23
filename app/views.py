0c19ef12f8201660a8b3edd31ed6b6b7bd3f54ef
majkizbajki <mrmajki552@gmail.com>
2021-04-24 01:06:20 +0200

    Zmiana danych użytkownika/hasła

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from app.models import News, Services, UserProfileInfo
 from app.forms import UserForm, UserProfileInfoForm
+from app.models import User
 
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
 def index(request):
@@ -112,6 +114,8 @@ def profile(request):
 
 def personal_data(request):
     u = request.user
+    user = User.objects.get(username=u.username)
+
     obj = UserProfileInfo.objects.get(user=u)
 
     gender = UserProfileInfo._meta.get_field("gender")
@@ -120,16 +124,94 @@ def personal_data(request):
     phone_number_value = phone_number.value_from_object(obj)
     birth_date = UserProfileInfo._meta.get_field("birth_date")
     birth_date_value = birth_date.value_from_object(obj)
+    error = ""
+    error_type = 0
 
-    return render(request,"app/personal_data.html",{"first_name_value":u.first_name,
-                                                    "last_name_value":u.last_name,
-                                                    "email_value":u.email,
+    if request.method == "POST":
+        request_first_name = request.POST.get("first_name")
+        request_last_name = request.POST.get("last_name")
+        request_email = request.POST.get("email")
+        request_phone_number = request.POST.get("phone_number")
+        request_birth_date = request.POST.get("birth_date")
+        request_gender = request.POST.get("gender")
+        error = ""
+        error_type = 0
+
+        if len(request_first_name)>1:
+            user.first_name = request_first_name
+        else:
+            error += "Niepoprawne imię. "
+            error_type = 1
+        if len(request_last_name)>1:
+            user.last_name = request_last_name
+        else:
+            error += "Niepoprawne nazwisko. "
+            error_type = 1
+        if User.objects.filter(email=request_email).count() == 0 and "@" in request_email and "." in request_email:
+            user.email = request_email
+        else:
+            error += "Niepoprawny lub już istniejący email. "
+            error_type = 1
+        if len(request_phone_number) == 9 and request_phone_number.isnumeric():
+            obj.phone_number = request_phone_number
+            phone_number_value = request_phone_number
+        else:
+            error += "Niepoprawny numer telefonu. "
+            error_type = 1
+        obj.birth_date = request_birth_date
+        birth_date_value = datetime.date(int(str(request_birth_date)[0]+str(request_birth_date)[1]+str(request_birth_date)[2]+str(request_birth_date)[3]),int(str(request_birth_date)[5]+str(request_birth_date)[6]),int(str(request_birth_date)[8]+str(request_birth_date)[9]))
+        obj.gender = request_gender
+        gender_value = request_gender
+
+        obj.save()
+        user.save()
+        if error_type == 0:
+            error += "Wprowadzone dane zapisano pomyślnie."
+            error_type = 2
+
+    return render(request,"app/personal_data.html",{"first_name_value":user.first_name,
+                                                    "last_name_value":user.last_name,
+                                                    "email_value":user.email,
                                                     "phone_number_value":phone_number_value,
                                                     "birth_date_value":birth_date_value,
-                                                    "gender_value":gender_value});
+                                                    "gender_value":gender_value,
+                                                    "error":error,
+                                                    "error_type": error_type});
 
 def change_password(request):
-    return render(request,"app/change_password.html");
+    u = request.user
+    user = User.objects.get(username=u.username)
+    error = ""
+    error_type = 0
+
+    if request.method == "POST":
+
+        current_password = request.POST.get("current")
+        new_password = request.POST.get("new")
+        repeat_password = request.POST.get("repeat")
+        error = ""
+        error_type = 0
+
+        if user.check_password(current_password):
+
+            if new_password == repeat_password:
+
+                user.set_password(new_password)
+                user.save()
+                update_session_auth_hash(request, user)
+
+                error += "Zmiana hasła przebiegła pomyślnie."
+                error_type = 2
+
+            else:
+                error += "Wpisane hasła różnią się od siebie. "
+                error_type = 1
+        else:
+            error += "Wprowadzone hasło nie jest zgodne z obecnym. "
+            error_type = 1
+
+    return render(request,"app/change_password.html",{"error":error,
+                                                      "error_type":error_type});
 
 def show_events(request):
     return render(request,"app/show_events.html");
